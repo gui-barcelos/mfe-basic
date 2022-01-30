360baaa5df258c845c8ffe2949e3e6dbe639f919
Guilherme Barcelos Monteiro Soares <guibarcelos@Guilhermes-MacBook-Pro.local>
2022-01-30 12:45:22 -0300

    use faker as a shared module

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,1 @@
-import faker from 'faker';
-
-let products = '';
-
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
-
-document.querySelector('#product-container').innerHTML = products;
+import('./bootstrap');
